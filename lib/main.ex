24597795a17372150a967b87cbb474f65faa30da
Rafeequl <rafeequl@gmail.com>
2025-08-22 06:46:27 +0700

    implement PONG

--- lib/main.ex
+++ lib/main.ex
@@ -16,12 +16,21 @@ defmodule Server do
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     IO.puts("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    #
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
+
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    case :gen_tcp.recv(client, 0) do
+      {:ok, data} ->
+        if String.contains?(data, "PING") do
+          :gen_tcp.send(client, "+PONG\r\n")
+        end
+        :gen_tcp.close(client)
+      {:error, _} ->
+        :gen_tcp.close(client)
+    end
   end
 end
 
