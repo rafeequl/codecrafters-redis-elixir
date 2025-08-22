8acfd5670282c4635d0dd536962155f7d60d2b05
Rafeequl Awan <rafeequl@seventhsky.id>
2025-08-22 10:57:09 +0700

    support multiple command

--- lib/main.ex
+++ lib/main.ex
@@ -26,16 +26,50 @@ defmodule Server do
   defp accept_connections(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
 
+    process_client_commands(client)
+
+    # After processing the request, accept the next connection
+    accept_connections(socket)
+  end
+
+  defp process_client_commands(client) do
     case :gen_tcp.recv(client, 0) do
       {:ok, data} ->
-        if String.contains?(data, "PING") do
-          :gen_tcp.send(client, "+PONG\r\n")
-        end
-      {:error, _} ->
+        IO.puts("Raw data: #{inspect(data)}")
+        
+        # Parse RESP format to extract commands
+        commands = parse_resp(data)
+        IO.puts("Parsed commands: #{inspect(commands)}")
+        
+        # Process each command
+        Enum.each(commands, fn command ->
+          if command == "PING" do
+            :gen_tcp.send(client, "+PONG\r\n")
+          end
+        end)
+        
+        # Continue processing more commands from this client
+        process_client_commands(client)
+        
+      {:error, :closed} ->
+        IO.puts("Client connection closed")
+        :gen_tcp.close(client)
+      {:error, reason} ->
+        IO.puts("Error receiving data from client: #{reason}")
         :gen_tcp.close(client)
     end
+  end
 
-    accept_connections(socket)
+  defp parse_resp(data) do
+    # Simple RESP parser for basic commands
+    # For "*1\r\n$4\r\nPING\r\n" format
+    case String.split(data, "\r\n", trim: true) do
+      ["*1", "$4", "PING"] -> ["PING"]
+      ["*1", "$4", "PING", ""] -> ["PING"]
+      _ -> 
+        IO.puts("Unknown RESP format: #{inspect(data)}")
+        []
+    end
   end
 end
 
