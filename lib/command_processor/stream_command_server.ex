363b5ee2a78e1f2dc557c54d9b9b4f9c9a5c2bfb
Rafeequl <rafeequl@seventhsky.id>
2025-09-17 09:44:08 +0700

    codecrafters submit [skip ci]

--- lib/command_processor/stream_command_server.ex
+++ lib/command_processor/stream_command_server.ex
@@ -18,16 +18,37 @@ defmodule CommandProcessor.StreamCommandServer do
     case GenServer.call(__MODULE__, {:xadd, stream_key, id, field_value_pairs}) do
       "OK" ->
         RESPFormatter.bulk_string(id)
+      error_message when is_binary(error_message) ->
+        RESPFormatter.error(error_message)
       _ ->
-        RESPFormatter.error("Error adding to stream")
+        RESPFormatter.error("Unknown error occurred")
     end
   end
 
   def handle_call({:xadd, stream_key, id, field_value_pairs}, _from, state) do
+    # validate if id is a valid id
+    # valid id is id that is equals or greater than exisiting id
     existing_stream = Map.get(state.streams, stream_key, [])
-    new_stream = existing_stream ++ [%{id: id, field_value_pairs: field_value_pairs}]
-    new_state = %{state | streams: Map.put(state.streams, stream_key, new_stream)}
-    {:reply, "OK", new_state}
+    if existing_stream == [] do
+      new_stream = existing_stream ++ [%{id: id, field_value_pairs: field_value_pairs}]
+      new_state = %{state | streams: Map.put(state.streams, stream_key, new_stream)}
+      {:reply, "OK", new_state}
+    else
+      existing_id = Enum.map(existing_stream, fn x -> x.id end)
+
+      cond do
+        id > Enum.max(existing_id) ->
+          new_stream = existing_stream ++ [%{id: id, field_value_pairs: field_value_pairs}]
+          new_state = %{state | streams: Map.put(state.streams, stream_key, new_stream)}
+          {:reply, "OK", new_state}
+        id == "0-0" ->
+          {:reply, "ERR The ID specified in XADD must be greater than 0-0", state}
+        id <= Enum.max(existing_id) ->
+          {:reply, "ERR The ID specified in XADD is equal or smaller than the target stream top item", state}
+        true ->
+          {:reply, "ERR The ID specified in XADD is equal or smaller than #{id}", state}
+      end
+    end
   end
 
   def handle_call({:exists, stream_key}, _from, state) do
